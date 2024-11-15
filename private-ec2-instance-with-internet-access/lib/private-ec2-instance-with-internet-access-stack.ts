97e83c0f032089d22bda35a0cad87cbfce9d3bf2
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-11-15 14:35:21 -0500

    private ec2 instance with internet access

---
+++ private-ec2-instance-with-internet-access/lib/private-ec2-instance-with-internet-access-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class PrivateEc2InstanceWithInternetAccessStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'PrivateEc2InstanceWithInternetAccessQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ private-ec2-instance-with-internet-access/test/private-ec2-instance-with-internet-access.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PrivateEc2InstanceWithInternetAccess from '../lib/private-ec2-instance-with-internet-access-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/private-ec2-instance-with-internet-access-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PrivateEc2InstanceWithInternetAccess.PrivateEc2InstanceWithInternetAccessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
