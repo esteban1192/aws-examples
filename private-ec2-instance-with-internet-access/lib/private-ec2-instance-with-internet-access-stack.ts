02416476659e5c8bcf10045449a6588029e434a8
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-11-16 18:06:00 -0500

    code improvement private-ec2-instance-with-internet-access-stack

--- private-ec2-instance-with-internet-access/lib/private-ec2-instance-with-internet-access-stack.ts
+++ private-ec2-instance-with-internet-access/lib/private-ec2-instance-with-internet-access-stack.ts
@@ -7,9 +7,8 @@ export class PrivateEc2InstanceWithInternetAccessStack extends cdk.Stack {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, 'MyVPC', {
-      createInternetGateway: true, // This is the default
-      maxAzs: 1, // Only one AZ for this example
-      subnetConfiguration: [ // 1 public and 1 private subnet for each AZ
+      maxAzs: 1,
+      subnetConfiguration: [
         {
           cidrMask: 24,
           name: 'PublicSubnet',
@@ -23,37 +22,35 @@ export class PrivateEc2InstanceWithInternetAccessStack extends cdk.Stack {
       ],
     });
 
-    const privateSubnet = vpc.selectSubnets({
-      subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
-    }).subnets[0];
+    const privateSubnet = vpc.selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }).subnets[0];
 
-    const securityGroup = new ec2.SecurityGroup(this, 'InstanceSG', {
+    const privateInstanceSG = new ec2.SecurityGroup(this, 'InstanceSG', {
       vpc,
-      allowAllOutbound: true, // This is the default
       description: 'Security group for private EC2 instance',
     });
 
-    const instance = new ec2.Instance(this, 'PrivateInstance', {
+    const privateInstance = new ec2.Instance(this, 'PrivateInstance', {
       vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO), // Free tier ec2 instance
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
       vpcSubnets: { subnets: [privateSubnet] },
-      securityGroup,
+      securityGroup: privateInstanceSG,
     });
 
     const instanceConnectSg = new ec2.SecurityGroup(this, 'InstanceConnectSG', {
       vpc,
       description: 'Security group for Instance Connect Endpoint',
-      allowAllOutbound: true,
     });
+
     new ec2.CfnInstanceConnectEndpoint(this, 'MyCfnInstanceConnectEndpoint', {
       subnetId: privateSubnet.subnetId,
-      securityGroupIds: [instanceConnectSg.securityGroupId]
+      securityGroupIds: [instanceConnectSg.securityGroupId],
     });
-    securityGroup.addIngressRule(instanceConnectSg, ec2.Port.tcp(22));
+
+    privateInstanceSG.addIngressRule(instanceConnectSg, ec2.Port.tcp(22));
 
     new cdk.CfnOutput(this, 'InstanceId', {
-      value: instance.instanceId,
+      value: privateInstance.instanceId,
       description: 'ID of the private EC2 instance',
     });
   }
