c1951327255f34b3e8f25422b7bc01b2da70f3dc
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-11-21 16:10:33 -0500

    vpc-peering-simple-stack cleaning

--- vpc-peering-simple/lib/vpc-peering-simple-stack.ts
+++ vpc-peering-simple/lib/vpc-peering-simple-stack.ts
@@ -2,79 +2,72 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
+import { Fn } from 'aws-cdk-lib';
 
 export class VpcPeeringSimpleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc1 = this.createVpc('VPC1', '10.0.0.0/16');
-    
-    const publicSubnet = (vpc1.publicSubnets[0] as ec2.Subnet)
+    const vpc1 = this.createInstanceVPC('VPC1', '10.0.0.0/16');
+    const publicSubnet = vpc1.publicSubnets[0] as ec2.Subnet;
 
-    const vpc2 = this.createVpc('VPC2', '10.1.0.0/16');
-    const privateSubnet2 = vpc2.isolatedSubnets[0] as ec2.Subnet
-    const privateSubnet3 = vpc2.isolatedSubnets[1] as ec2.Subnet;
-
-    let subnets = [];
-    if(privateSubnet2) subnets.push(privateSubnet2)
-    if(privateSubnet3) subnets.push(privateSubnet3)
-
-    const subnetGroup = this.createRdsSubnetGroup('VPC1PrivateSubnetGroup', vpc2, subnets);
+    const vpc2 = this.createDatabaseVpc('VPC2', '10.1.0.0/16');
+    const vpc2IsolatedSubnets = vpc2.isolatedSubnets as ec2.Subnet[];
 
     const vpcPeering = this.createVpcPeering('VpcPeering', vpc1, vpc2);
 
     this.addVpcPeeringRoutes(publicSubnet, vpc2.vpcCidrBlock, vpcPeering);
-    if(privateSubnet2) this.addVpcPeeringRoutes(privateSubnet2, vpc1.vpcCidrBlock, vpcPeering);
-    if(privateSubnet3) this.addVpcPeeringRoutes(privateSubnet3, vpc1.vpcCidrBlock, vpcPeering);
+    vpc2IsolatedSubnets.forEach((subnet) => {
+      this.addVpcPeeringRoutes(subnet, vpc1.vpcCidrBlock, vpcPeering)
+    });
 
     const instanceSecurityGroup = this.createSecurityGroup('InstanceSG', vpc1, 'Allow SSH access', 22);
     const dbSecurityGroup = this.createDbSecurityGroup('DbSG', vpc2, instanceSecurityGroup);
 
-    this.createEc2Instance('EC2Instance', vpc1, publicSubnet, instanceSecurityGroup);
+    const databaseSubnetGroup = this.createRdsSubnetGroup('VPC2PrivateSubnetGroup', vpc2, vpc2IsolatedSubnets);
 
-    this.createRdsInstance('MySQLRDS', vpc2, dbSecurityGroup, subnetGroup);
+    this.createEc2Instance('EC2Instance', vpc1, publicSubnet, instanceSecurityGroup);
+    this.createRdsInstance('MySQLRDS', vpc2, dbSecurityGroup, databaseSubnetGroup);
 
     new cdk.CfnOutput(this, 'Vpc1Id', { value: vpc1.vpcId });
     new cdk.CfnOutput(this, 'Vpc2Id', { value: vpc2.vpcId });
     new cdk.CfnOutput(this, 'VpcPeeringId', { value: vpcPeering.ref });
   }
 
-  private createVpc(id: string, cidr: string): ec2.Vpc {
+  private createDatabaseVpc(id: string, cidr: string): ec2.Vpc {
     return new ec2.Vpc(this, id, {
       cidr,
-      maxAzs: 2,
-      subnetConfiguration: [  
-        {
-          cidrMask: 24,
-          name: 'public',
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
+      maxAzs: 2, //required for RDS subnet group
+      natGateways: 0,
+      subnetConfiguration: [
         {
           cidrMask: 24,
           name: 'private',
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-        }
-      ]
+        },
+      ],
     });
   }
 
-  private createSubnet(vpc: ec2.Vpc, id: string, cidrBlock: string, availabilityZone: string, publicSubnet: boolean): ec2.PrivateSubnet {
-    if(publicSubnet) return new ec2.PublicSubnet(this, id, {
-      vpcId: vpc.vpcId,
-      cidrBlock,
-      availabilityZone,
-    });
-    return new ec2.PrivateSubnet(this, id, {
-      vpcId: vpc.vpcId,
-      cidrBlock,
-      availabilityZone,
+  private createInstanceVPC(id: string, cidr: string): ec2.Vpc {
+    return new ec2.Vpc(this, id, {
+      cidr,
+      maxAzs: 1,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ],
     });
   }
 
   private createRdsSubnetGroup(id: string, vpc: ec2.Vpc, subnets: ec2.Subnet[]): rds.SubnetGroup {
     return new rds.SubnetGroup(this, id, {
       vpc,
-      description: 'The private subnet to place the RDS instance',
+      description: 'The private subnets for the RDS instance',
       vpcSubnets: { subnets },
     });
   }
@@ -86,7 +79,7 @@ export class VpcPeeringSimpleStack extends cdk.Stack {
     });
   }
 
-  private addVpcPeeringRoutes(subnet: ec2.PrivateSubnet, destinationCidr: string, vpcPeering: ec2.CfnVPCPeeringConnection) {
+  private addVpcPeeringRoutes(subnet: ec2.Subnet, destinationCidr: string, vpcPeering: ec2.CfnVPCPeeringConnection) {
     subnet.addRoute(`${subnet.node.id}RouteToPeering`, {
       destinationCidrBlock: destinationCidr,
       routerId: vpcPeering.attrId,
@@ -110,25 +103,33 @@ export class VpcPeeringSimpleStack extends cdk.Stack {
       allowAllOutbound: true,
       securityGroupName: id,
     });
-    dbSecurityGroup.addIngressRule(ec2.Peer.securityGroupId(instanceSecurityGroup.securityGroupId), ec2.Port.tcp(3306), 'Allow MySQL access from EC2 instance');
+    dbSecurityGroup.addIngressRule(
+      ec2.Peer.securityGroupId(instanceSecurityGroup.securityGroupId),
+      ec2.Port.tcp(3306),
+      'Allow MySQL access from EC2 instance'
+    );
     return dbSecurityGroup;
   }
 
   private createEc2Instance(id: string, vpc: ec2.Vpc, subnet: ec2.ISubnet, securityGroup: ec2.SecurityGroup) {
+    const userData = ec2.UserData.forLinux()
+    userData.addCommands(
+      'sudo dnf install -y mariadb105'
+    );
     new ec2.Instance(this, id, {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023 }),
       vpc,
       securityGroup,
       vpcSubnets: { subnets: [subnet] },
+      userData: userData,
+      userDataCausesReplacement: true
     });
   }
 
   private createRdsInstance(id: string, vpc: ec2.Vpc, securityGroup: ec2.SecurityGroup, subnetGroup: rds.SubnetGroup) {
     new rds.DatabaseInstance(this, id, {
-      engine: rds.DatabaseInstanceEngine.mysql({
-        version: rds.MysqlEngineVersion.VER_8_0_35,
-      }),
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_35 }),
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       vpc,
       securityGroups: [securityGroup],
