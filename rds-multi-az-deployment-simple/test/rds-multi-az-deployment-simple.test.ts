b30f730fe5262a05e40998a1395667accb4a00fb
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-11-23 12:42:00 -0500

    RDS simple multi az deployment stack initial commit

---
+++ rds-multi-az-deployment-simple/test/rds-multi-az-deployment-simple.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RdsMultiAzDeploymentSimple from '../lib/rds-multi-az-deployment-simple-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rds-multi-az-deployment-simple-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RdsMultiAzDeploymentSimple.RdsMultiAzDeploymentSimpleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
