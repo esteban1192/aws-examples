74f3665ad8a834523c662158d3123ab4a58b0ff2
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-11-27 13:01:31 -0500

    transit-gateway-simple-vpc-connections-stack add transit gateway and attachments

--- transit-gateway-simple-vpc-connections/lib/transit-gateway-simple-vpc-connections-stack.ts
+++ transit-gateway-simple-vpc-connections/lib/transit-gateway-simple-vpc-connections-stack.ts
@@ -6,13 +6,29 @@ export class TransitGatewaySimpleVpcConnectionsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const transitGateway = this.createTransitGateway();
+
     const vpc0 = this.createVpc('VPC0', '10.0.0.0/16');
     const vpc1 = this.createVpc('VPC1', '10.1.0.0/16');
     const vpc2 = this.createVpc('VPC2', '10.2.0.0/16');
 
-    this.addEc2InstanceToIsolatedSubnets(vpc0);
-    this.addEc2InstanceToIsolatedSubnets(vpc1);
-    this.addEc2InstanceToIsolatedSubnets(vpc2);
+    this.createTransitGatewayAttachment(transitGateway, vpc0, 'Attachment-VPC0');
+    this.createTransitGatewayAttachment(transitGateway, vpc1, 'Attachment-VPC1');
+    this.createTransitGatewayAttachment(transitGateway, vpc2, 'Attachment-VPC2');
+
+    this.addEc2InstanceToIsolatedSubnet(vpc0);
+    this.addEc2InstanceToIsolatedSubnet(vpc1);
+    this.addEc2InstanceToIsolatedSubnet(vpc2);
+  }
+
+  private createTransitGateway(): ec2.CfnTransitGateway {
+    return new ec2.CfnTransitGateway(this, 'MyTransitGateway', {
+      amazonSideAsn: 64512,
+      description: 'A central Transit Gateway for private subnet attachments',
+      autoAcceptSharedAttachments: 'enable',
+      defaultRouteTableAssociation: 'enable',
+      defaultRouteTablePropagation: 'enable',
+    });
   }
 
   private createVpc(id: string, cidrBlock: string): ec2.Vpc {
@@ -30,8 +46,20 @@ export class TransitGatewaySimpleVpcConnectionsStack extends cdk.Stack {
     });
   }
 
-  private addEc2InstanceToIsolatedSubnets(vpc: ec2.Vpc): void {
-    new ec2.Instance(this, `Ec2Instance-${vpc.vpcId}`, {
+  private createTransitGatewayAttachment(
+    transitGateway: ec2.CfnTransitGateway,
+    vpc: ec2.Vpc,
+    attachmentId: string
+  ): void {
+    new ec2.CfnTransitGatewayAttachment(this, attachmentId, {
+      subnetIds: vpc.isolatedSubnets.map(subnet => subnet.subnetId),
+      transitGatewayId: transitGateway.attrId,
+      vpcId: vpc.vpcId,
+    });
+  }
+
+  private addEc2InstanceToIsolatedSubnet(vpc: ec2.Vpc) {
+    new ec2.Instance(this, `Ec2Instance-${vpc.node.id}`, {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023 }),
       vpc,
