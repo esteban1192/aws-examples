f63e872a6813b60cd3a691d42a621d7ba82326df
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-11-28 08:57:18 -0500

    serverless-simple-image-processing cdk init and diagram

---
+++ serverless-simple-image-processing/test/serverless-simple-image-processing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessSimpleImageProcessing from '../lib/serverless-simple-image-processing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-simple-image-processing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessSimpleImageProcessing.ServerlessSimpleImageProcessingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
