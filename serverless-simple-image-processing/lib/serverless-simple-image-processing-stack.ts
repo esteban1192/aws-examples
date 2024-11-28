c0c58a4bf888f9113beff097ad852bd525c4dc77
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-11-28 13:23:40 -0500

    image uploader, thumbnails bucket and api gateway

--- serverless-simple-image-processing/lib/serverless-simple-image-processing-stack.ts
+++ serverless-simple-image-processing/lib/serverless-simple-image-processing-stack.ts
@@ -1,27 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
 export class ServerlessSimpleImageProcessingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'ThumbnailsBucket', {
+    const originalsBucket = new s3.Bucket(this, 'OriginalsBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for dev environments
-      bucketName: `thumbnails-bucket-${this.account}`
+      bucketName: `originals-bucket-${this.account}`,
     });
+
+    const thumbnailsBucket = new s3.Bucket(this, 'ThumbnailsBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for dev environments
+      bucketName: `thumbnails-bucket-${this.account}`,
+    });
+
+    // CloudFront setup
     const oac = new cloudfront.S3OriginAccessControl(this, 'S3OriginAccessControl', {
       originAccessControlName: 'S3OriginAccessControl',
     });
-    const s3Origin = origins.S3BucketOrigin.withOriginAccessControl(bucket, {
-      originAccessControl: oac
-    })
+    const s3Origin = origins.S3BucketOrigin.withOriginAccessControl(thumbnailsBucket, {
+      originAccessControl: oac,
+    });
     new cloudfront.Distribution(this, 'CloudfrontDistribution', {
       defaultBehavior: {
-        origin: s3Origin
+        origin: s3Origin,
       },
     });
+
+    // Lambda Function to handle image upload
+    const layerVersion = new lambda.LayerVersion(this, 'UploadHandlerLayerVersion', {
+      code: lambda.Code.fromAsset(path.join(__dirname, 'image-uploader', 'layer-version')),
+    })
+    const uploadHandler = new lambda.Function(this, 'UploadHandler', {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'image-uploader')),
+      environment: {
+        BUCKET_NAME: originalsBucket.bucketName,
+      },
+      layers: [layerVersion]
+    });
+    originalsBucket.grantPut(uploadHandler);
+
+    const api = new apigateway.RestApi(this, 'ImageUploadApi', {
+      restApiName: 'Image Upload API',
+    });
+
+    const imageResource = api.root.addResource('image');
+    imageResource.addMethod('POST', new apigateway.LambdaIntegration(uploadHandler));
   }
 }
