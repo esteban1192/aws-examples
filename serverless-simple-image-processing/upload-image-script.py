60b101a6215c17c9277640ba34dfa501d10f1d38
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-11-29 13:10:12 -0500

    update upload image script

--- serverless-simple-image-processing/upload-image-script.py
+++ serverless-simple-image-processing/upload-image-script.py
@@ -5,16 +5,19 @@ def encode_image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
-api_url = "https://t4yh617h27.execute-api.us-east-1.amazonaws.com/prod/image" # Replace this with your own api gateway domain
+api_url = "https://2zt2gqv0rh.execute-api.us-east-1.amazonaws.com/prod/image"
 
-image_path = "test-images/tree-736885_1280.jpg"
+file_name = 'tree-3097419_960_720.jpg'
+image_path = f"test-images/{file_name}"
 
 try:
     base64_image_content = encode_image_to_base64(image_path)
+    
     payload = {
-        "imageName": "example.jpg",  # Name of the image in S3
+        "imageName": file_name,
         "imageContent": base64_image_content
     }
+    
     headers = {
         "Content-Type": "application/json"
     }
