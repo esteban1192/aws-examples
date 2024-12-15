b82f5294e78da5e52a4dbd80b323a03cc1be50f6
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-15 13:32:39 -0500

    cloudtratil-multitrail-stack added s3 buckets plus data event trail

--- cloudtrail-multitrail-simple/lib/cloudtrail-multitrail-simple-stack.ts
+++ cloudtrail-multitrail-simple/lib/cloudtrail-multitrail-simple-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudtrail from 'aws-cdk-lib/aws-cloudtrail';
 
 export class CloudtrailMultitrailSimpleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const loggingBucket = new s3.Bucket(this, 'LoggingBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudtrailMultitrailSimpleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const watchedBucket = new s3.Bucket(this, 'WatchedBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    const dataEventTrail = new cloudtrail.Trail(this, 'DataEventTrail', {
+      bucket: loggingBucket,
+      includeGlobalServiceEvents: false,  // Do not include global service events
+      managementEvents: cloudtrail.ReadWriteType.NONE, // Only log data events, not management events
+      sendToCloudWatchLogs: true,
+      isMultiRegionTrail: false,
+    });
+
+    dataEventTrail.addS3EventSelector(
+      [{ bucket: watchedBucket }],
+      { readWriteType: cloudtrail.ReadWriteType.ALL }
+    );
   }
 }
