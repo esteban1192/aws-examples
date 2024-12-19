656df58a594ca7f9a0233686d48d67805c2a92fd
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-19 12:20:02 -0500

    added cluster and vpc

--- aurora-global-database-simple/lib/aurora-global-database-simple-stack.ts
+++ aurora-global-database-simple/lib/aurora-global-database-simple-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as aws_rds from 'aws-cdk-lib/aws-rds';
+import * as aws_ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class AuroraGlobalDatabaseSimpleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new aws_ec2.Vpc(this, 'AuroraVpc', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'PrivateSubnet',
+          subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuroraGlobalDatabaseSimpleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const mainCluster = new aws_rds.DatabaseCluster(this, 'DatabaseCluster', {
+      engine: aws_rds.DatabaseClusterEngine.auroraMysql({
+        version: aws_rds.AuroraMysqlEngineVersion.VER_3_08_0,
+      }),
+      vpc: vpc,
+      vpcSubnets: {
+        subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      writer: aws_rds.ClusterInstance.provisioned('ClusterWriterInstance', {
+        instanceType: aws_ec2.InstanceType.of(aws_ec2.InstanceClass.R5, aws_ec2.InstanceSize.LARGE)
+      })
+    });
+
+    const globalCluster = new aws_rds.CfnGlobalCluster(this, 'GlobalCluster', {
+      sourceDbClusterIdentifier: mainCluster.clusterIdentifier,
+    });
   }
 }
