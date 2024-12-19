352acb54a1f4a9f26470c9093f1fc45ce84668f4
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-18 19:40:00 -0500

    aurora-global-database initial commit

---
+++ aurora-global-database-simple/test/aurora-global-database-simple.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AuroraGlobalDatabaseSimple from '../lib/aurora-global-database-simple-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aurora-global-database-simple-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AuroraGlobalDatabaseSimple.AuroraGlobalDatabaseSimpleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
