b6d6cc3a84a44abca32bfaec160cff761a5769d0
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-20 12:33:33 -0500

    Deploy both main and secondary clusters

--- aurora-global-database-simple/bin/aurora-global-database-simple.ts
+++ aurora-global-database-simple/bin/aurora-global-database-simple.ts
@@ -2,20 +2,18 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AuroraGlobalDatabaseSimpleStack } from '../lib/aurora-global-database-simple-stack';
+import { SecondaryClusterStack } from '../lib/secondary-cluster-stack';
 
 const app = new cdk.App();
-new AuroraGlobalDatabaseSimpleStack(app, 'AuroraGlobalDatabaseSimpleStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const globalDatabaseStack = new AuroraGlobalDatabaseSimpleStack(app, 'AuroraGlobalDatabaseSimpleStack', {
+  env: {
+    region: 'us-east-1'
+  }
+});
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new SecondaryClusterStack(app, 'SecondaryClusterStack', {
+  env: {
+    region: 'us-east-2'
+  },
+  globalCluster: globalDatabaseStack.getGlobalCluster()
 });
