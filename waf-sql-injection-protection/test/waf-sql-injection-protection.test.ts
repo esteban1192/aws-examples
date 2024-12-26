03d4ef88ac8361ba0a7d4dc244fcdcca43724f2a
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-26 14:09:30 -0500

    waf-sql-injection-protection initial commit

---
+++ waf-sql-injection-protection/test/waf-sql-injection-protection.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WafSqlInjectionProtection from '../lib/waf-sql-injection-protection-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/waf-sql-injection-protection-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WafSqlInjectionProtection.WafSqlInjectionProtectionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
