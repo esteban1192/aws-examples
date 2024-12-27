00e0279b27e0059de4650650c787d8432aab41e4
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-27 15:12:15 -0500

    enhance test

--- waf-sql-injection-protection/test-fetch.js
+++ waf-sql-injection-protection/test-fetch.js
@@ -1,28 +1,17 @@
 async function testVulnerableEndpoint() {
-  try {
-    const response = await fetch('https://vindk0gyuh.execute-api.us-east-1.amazonaws.com/prod/vulnerable-endpoint', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ param: "' OR 1=1 --" })
-    });
+  const response = await fetch('https://vindk0gyuh.execute-api.us-east-1.amazonaws.com/prod/vulnerable-endpoint', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ param: "' R 1=1 --" })
+  });
 
-    console.log("Status:", response.status);
+  console.log("Status:", response.status);
 
-    // Wait for the response body and log it
-    const data = await response.text();
-    console.log("Response Data:", data);
+  const data = await response.json();
 
-    // Optionally, assert or check the response for specific test conditions
-    if (response.ok) {
-      console.log("Request was successful");
-    } else {
-      console.error("Request failed");
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  console.log("Response Data:", data);
 }
 
 testVulnerableEndpoint();
