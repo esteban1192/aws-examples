39accf0ad86ad884882a88a9c35a59921e7e2eb7
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-27 14:26:20 -0500

    add test fetch script

---
+++ waf-sql-injection-protection/test-fetch.js
@@ -0,0 +1,28 @@
+async function testVulnerableEndpoint() {
+  try {
+    const response = await fetch('https://vindk0gyuh.execute-api.us-east-1.amazonaws.com/prod/vulnerable-endpoint', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ param: "Wazaaaa" })
+    });
+
+    console.log("Status:", response.status);
+
+    // Wait for the response body and log it
+    const data = await response.text();
+    console.log("Response Data:", data);
+
+    // Optionally, assert or check the response for specific test conditions
+    if (response.ok) {
+      console.log("Request was successful");
+    } else {
+      console.error("Request failed");
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+testVulnerableEndpoint();
