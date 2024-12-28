f459b921fd443f895da3ecd418ee3c4d09616422
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-28 13:53:27 -0500

    update test fetch

--- waf-sql-injection-protection/test-fetch.js
+++ waf-sql-injection-protection/test-fetch.js
@@ -1,10 +1,10 @@
 async function testVulnerableEndpoint() {
-  const response = await fetch('https://vindk0gyuh.execute-api.us-east-1.amazonaws.com/prod/vulnerable-endpoint', {
+  const response = await fetch('https://avy5samxsc.execute-api.us-east-1.amazonaws.com/prod/vulnerable-endpoint', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ param: "' R 1=1 --" })
+    body: JSON.stringify({ "id": "1 UNION SELECT user_id, ssn from sensitive_data --" })
   });
 
   console.log("Status:", response.status);
