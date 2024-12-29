26b017b33206afc263131fe69c9d6a1010f6e563
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-29 18:41:53 -0500

    s3-vpc-gateway-endpoint initial commit

---
+++ s3-vpc-gateway-endpoint/test/s3-vpc-gateway-endpoint.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3VpcGatewayEndpoint from '../lib/s3-vpc-gateway-endpoint-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-vpc-gateway-endpoint-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3VpcGatewayEndpoint.S3VpcGatewayEndpointStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
