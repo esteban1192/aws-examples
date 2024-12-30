cbdb6d8bfdb2b5b4f7f10c09a55e087395c93191
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-29 20:03:49 -0500

    bucket, vpc and endpoint added

--- s3-vpc-gateway-endpoint/lib/s3-vpc-gateway-endpoint-stack.ts
+++ s3-vpc-gateway-endpoint/lib/s3-vpc-gateway-endpoint-stack.ts
@@ -1,8 +1,50 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
+import * as path from 'path';
 
 export class S3VpcGatewayEndpointStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'Isolated',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const lambdaFunction = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-function')),
+      vpc: vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
+
+    bucket.grantRead(lambdaFunction);
+
+    const s3BucketEndpoint = new ec2.GatewayVpcEndpoint(this, 'VpcEndpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.S3,
+      vpc: vpc
+    });
+
+    s3BucketEndpoint.addToPolicy({
+      
+    })
   }
 }
