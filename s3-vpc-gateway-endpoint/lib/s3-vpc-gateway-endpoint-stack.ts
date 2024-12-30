d5c3be5d56c2ef20a0ad3dc5b8e915daf6d4721e
esteban1192 <estebanospinasaldarriaga@gmail.com>
2024-12-29 23:06:22 -0500

    use lambda role instead of function directly

--- s3-vpc-gateway-endpoint/lib/s3-vpc-gateway-endpoint-stack.ts
+++ s3-vpc-gateway-endpoint/lib/s3-vpc-gateway-endpoint-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as iam from 'aws-cdk-lib/aws-iam'
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import * as path from 'path';
 
@@ -18,13 +18,32 @@ export class S3VpcGatewayEndpointStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
       ],
+      createInternetGateway: false
     });
 
     const bucket = new s3.Bucket(this, 'MyBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
-    const lambdaFunction = new lambda.Function(this, 'MyLambda', {
+    const lambdaRole = new iam.Role(this, 'LambdaExecutionRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        S3AccessPolicy: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              actions: ['s3:GetObject', 's3:ListBucket'],
+              resources: [
+                `${bucket.bucketArn}`,
+                `${bucket.bucketArn}/*`,
+              ],
+            }),
+          ],
+        }),
+      },
+    });
+
+    new lambda.Function(this, 'MyLambda', {
       runtime: lambda.Runtime.NODEJS_22_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-function')),
@@ -35,21 +54,17 @@ export class S3VpcGatewayEndpointStack extends cdk.Stack {
       environment: {
         BUCKET_NAME: bucket.bucketName,
       },
+      role: lambdaRole,
     });
-
-    bucket.grantRead(lambdaFunction);
 
     const s3BucketEndpoint = new ec2.GatewayVpcEndpoint(this, 'VpcEndpoint', {
       service: ec2.GatewayVpcEndpointAwsService.S3,
       vpc: vpc,
     });
-    
-    console.log("bucketarn", bucket.bucketArn);
-    
 
     /**
      * https://docs.aws.amazon.com/vpc/latest/privatelink/vpc-endpoints-access.html
-     * With gateway endpoints, the Principal element must be set to *. To specify a principal, use the aws:PrincipalArn condition key.
+     * As of today that link says: "With gateway endpoints, the Principal element must be set to *. To specify a principal, use the aws:PrincipalArn condition key."
      */
     s3BucketEndpoint.addToPolicy(
       new iam.PolicyStatement({
@@ -62,11 +77,10 @@ export class S3VpcGatewayEndpointStack extends cdk.Stack {
         ],
         conditions: {
           StringEquals: {
-            'aws:PrincipalArn': lambdaFunction.functionArn,
+            'aws:PrincipalArn': lambdaRole.roleArn,
           },
         },
       })
     );
-    
   }
 }
