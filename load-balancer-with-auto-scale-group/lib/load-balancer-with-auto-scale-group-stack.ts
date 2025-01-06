dfb90bd58c17af0b8cb99f197e7b8669852a5a38
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-01-06 15:45:06 -0500

    load-balancer-with-auto-scale-group-stack updates

--- load-balancer-with-auto-scale-group/lib/load-balancer-with-auto-scale-group-stack.ts
+++ load-balancer-with-auto-scale-group/lib/load-balancer-with-auto-scale-group-stack.ts
@@ -1,9 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class LoadBalancerWithAutoScaleGroupStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,17 +11,15 @@ export class LoadBalancerWithAutoScaleGroupStack extends cdk.Stack {
 
     const vpc = this.createVpc();
 
-    const instanceConnectSg = this.createInstanceConnectSecurityGroup(vpc);
-    this.createInstanceConnectEndpoint(vpc, instanceConnectSg);
-
     const albSecurityGroup = this.createAlbSecurityGroup(vpc);
-    const ec2SecurityGroup = this.createEc2SecurityGroup(vpc, albSecurityGroup, instanceConnectSg);
-
     const alb = this.createAlb(vpc, albSecurityGroup);
 
-    const asg = this.createAutoScalingGroup(vpc, ec2SecurityGroup);
+    const asgSecurityGroup = this.createEc2SecurityGroupForAsg(vpc, albSecurityGroup);
+    const asg = this.createAutoScalingGroup(vpc, asgSecurityGroup);
 
-    this.addAlbListenerAndTargets(alb, asg);
+    this.setupAlbListenerAndTargets(alb, asg);
+
+    this.addVpcEndpoints(vpc);
 
     this.outputAlbDns(alb);
   }
@@ -33,96 +31,87 @@ export class LoadBalancerWithAutoScaleGroupStack extends cdk.Stack {
       subnetConfiguration: [
         {
           name: 'Public',
-          subnetType: ec2.SubnetType.PUBLIC,
+          subnetType: ec2.SubnetType.PUBLIC
         },
         {
           name: 'Private',
-          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
         },
       ],
     });
   }
 
-  private createInstanceConnectSecurityGroup(vpc: ec2.Vpc): ec2.SecurityGroup {
-    const sg = new ec2.SecurityGroup(this, 'InstanceConnectSG', {
+  private addVpcEndpoints(vpc: ec2.Vpc): void {
+    new ec2.InterfaceVpcEndpoint(this, 'SSMEndpoint', {
       vpc,
-      description: 'Security group for Instance Connect Endpoint',
-    });
-    return sg;
-  }
-
-  private createInstanceConnectEndpoint(vpc: ec2.Vpc, securityGroup: ec2.SecurityGroup): ec2.CfnInstanceConnectEndpoint {
-    const subnetId = vpc.selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }).subnets[0].subnetId;
-    return new ec2.CfnInstanceConnectEndpoint(this, 'MyCfnInstanceConnectEndpoint', {
-      subnetId,
-      securityGroupIds: [securityGroup.securityGroupId],
+      service: ec2.InterfaceVpcEndpointAwsService.SSM,
     });
   }
 
   private createAlbSecurityGroup(vpc: ec2.Vpc): ec2.SecurityGroup {
-    const sg = new ec2.SecurityGroup(this, 'AlbSecurityGroup', {
-      vpc,
-    });
-    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic');
+    const sg = new ec2.SecurityGroup(this, 'AlbSecurityGroup', { vpc });
+    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow inbound HTTP traffic');
     return sg;
   }
 
-  private createEc2SecurityGroup(vpc: ec2.Vpc, albSecurityGroup: ec2.SecurityGroup, instanceConnectEndpointSG: ec2.SecurityGroup): ec2.SecurityGroup {
-    const sg = new ec2.SecurityGroup(this, 'Ec2SecurityGroup', {
-      vpc,
-    });
-    sg.addIngressRule(albSecurityGroup, ec2.Port.tcp(80), 'Allow HTTP traffic from ALB');
-    sg.addIngressRule(instanceConnectEndpointSG, ec2.Port.tcp(22), 'Allow ssh connections from instance connect endpoint');
-    return sg;
-  }
-
-  private createAlb(vpc: ec2.Vpc, securityGroup: ec2.SecurityGroup): elbv2.ApplicationLoadBalancer {
-    return new elbv2.ApplicationLoadBalancer(this, 'ALB', {
+  private createAlb(vpc: ec2.Vpc, albSecurityGroup: ec2.SecurityGroup): elbv2.ApplicationLoadBalancer {
+    return new elbv2.ApplicationLoadBalancer(this, 'ApplicationLoadBalancer', {
       vpc,
       internetFacing: true,
-      securityGroup,
+      securityGroup: albSecurityGroup,
     });
   }
 
-  private createAutoScalingGroup(vpc: ec2.Vpc, securityGroup: ec2.SecurityGroup): autoscaling.AutoScalingGroup {
+  private createEc2SecurityGroupForAsg(vpc: ec2.Vpc, albSecurityGroup: ec2.SecurityGroup): ec2.SecurityGroup {
+    const sg = new ec2.SecurityGroup(this, 'Ec2SecurityGroup', { vpc });
+    sg.addIngressRule(albSecurityGroup, ec2.Port.tcp(80), 'Allow traffic from ALB');
+    return sg;
+  }
+  private createAutoScalingGroup(vpc: ec2.Vpc, asgSecurityGroup: ec2.SecurityGroup): autoscaling.AutoScalingGroup {
+    const role = new iam.Role(this, 'Ec2InstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')],
+    });
+
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      'sudo dnf upgrade -y',
+      'sudo dnf install -y httpd stress',
+      'sudo systemctl enable httpd',
+      'sudo systemctl start httpd',
+      `echo "$HOSTNAME" | sudo tee /var/www/html/index.html`
+    );
+
     const autoScalingGroup = new autoscaling.AutoScalingGroup(this, 'AutoScalingGroup', {
       vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
-      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
+      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023 }),
       minCapacity: 1,
       desiredCapacity: 1,
       maxCapacity: 3,
-      securityGroup,
-      userData: ec2.UserData.custom(`
-        #!/bin/bash
-        sudo amazon-linux-extras install epel
-        sudo yum install -y stress
-        sudo yum install -y httpd
-        sudo systemctl enable httpd
-        sudo systemctl start httpd
-        sudo sh -c 'echo "$HOSTNAME" > /var/www/html/index.html'
-      `),
+      securityGroup: asgSecurityGroup,
+      role,
+      userData,
       vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
     });
-    autoScalingGroup.scaleOnCpuUtilization('KeepSpareCPU', {
+    autoScalingGroup.scaleOnCpuUtilization('CPUUtilizationScalingPolicy', {
       targetUtilizationPercent: 50
     });
     return autoScalingGroup;
   }
 
-  private addAlbListenerAndTargets(alb: elbv2.ApplicationLoadBalancer, asg: autoscaling.AutoScalingGroup): void {
-    const listener = alb.addListener('Listener', {
+  private setupAlbListenerAndTargets(alb: elbv2.ApplicationLoadBalancer, asg: autoscaling.AutoScalingGroup): void {
+    const listener = alb.addListener('HttpListener', { port: 80 });
+    listener.addTargets('HttpTargetGroup', {
       port: 80,
-    });
-    listener.addTargets('TargetGroup', {
-      port: 80,
-      targets: [asg],
+      targets: [asg]
     });
   }
 
   private outputAlbDns(alb: elbv2.ApplicationLoadBalancer): void {
     new cdk.CfnOutput(this, 'LoadBalancerDNS', {
       value: alb.loadBalancerDnsName,
+      description: 'The DNS name of the load balancer',
     });
   }
 }
