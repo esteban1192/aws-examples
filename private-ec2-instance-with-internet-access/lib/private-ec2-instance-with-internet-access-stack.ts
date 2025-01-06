fbfa51c8c1cfec961b335844994df26e48da1619
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-01-06 14:25:12 -0500

    update

--- private-ec2-instance-with-internet-access/lib/private-ec2-instance-with-internet-access-stack.ts
+++ private-ec2-instance-with-internet-access/lib/private-ec2-instance-with-internet-access-stack.ts
@@ -1,57 +1,60 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class PrivateEc2InstanceWithInternetAccessStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create the VPC
+    const subnetConfiguration: ec2.SubnetConfiguration[] = [
+      {
+        cidrMask: 24,
+        name: 'PublicSubnet',
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      {
+        cidrMask: 24,
+        name: 'PrivateSubnet',
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+    ];
     const vpc = new ec2.Vpc(this, 'MyVPC', {
+      createInternetGateway: true,
       maxAzs: 1,
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: 'PublicSubnet',
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: 'PrivateSubnet',
-          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-        },
+      natGatewayProvider: ec2.NatProvider.gateway(),
+      natGateways: 1,
+      subnetConfiguration,
+    });
+
+    // Add the EC2 instance
+    const subnetSelection: ec2.SubnetSelection = {
+      subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+    };
+    const instanceRole = new iam.Role(this, 'InstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
       ],
     });
-
-    const privateSubnet = vpc.selectSubnets({ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }).subnets[0];
-
-    const privateInstanceSG = new ec2.SecurityGroup(this, 'InstanceSG', {
-      vpc,
-      description: 'Security group for private EC2 instance',
-    });
-
-    const privateInstance = new ec2.Instance(this, 'PrivateInstance', {
+    new ec2.Instance(this, 'PrivateInstance', {
       vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
-      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
-      vpcSubnets: { subnets: [privateSubnet] },
-      securityGroup: privateInstanceSG,
+      machineImage: new ec2.AmazonLinuxImage({ 
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023 
+      }),
+      vpcSubnets: subnetSelection,
+      role: instanceRole
     });
 
-    const instanceConnectSg = new ec2.SecurityGroup(this, 'InstanceConnectSG', {
+    /**
+     * Add an interface endpoint to
+     * connect using Session Manager
+     */
+    new ec2.InterfaceVpcEndpoint(this, 'SSMEndpoint', {
       vpc,
-      description: 'Security group for Instance Connect Endpoint',
-    });
-
-    new ec2.CfnInstanceConnectEndpoint(this, 'MyCfnInstanceConnectEndpoint', {
-      subnetId: privateSubnet.subnetId,
-      securityGroupIds: [instanceConnectSg.securityGroupId],
-    });
-
-    privateInstanceSG.addIngressRule(instanceConnectSg, ec2.Port.tcp(22));
-
-    new cdk.CfnOutput(this, 'InstanceId', {
-      value: privateInstance.instanceId,
-      description: 'ID of the private EC2 instance',
+      service: ec2.InterfaceVpcEndpointAwsService.SSM
     });
   }
 }
