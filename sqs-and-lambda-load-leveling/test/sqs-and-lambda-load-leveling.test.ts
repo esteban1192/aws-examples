0156839d85f21c86232e73deeddb69eda422d9a8
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-01-08 21:36:22 -0500

    sqs load leveling initial commit

---
+++ sqs-and-lambda-load-leveling/test/sqs-and-lambda-load-leveling.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SqsAndLambdaLoadLeveling from '../lib/sqs-and-lambda-load-leveling-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sqs-and-lambda-load-leveling-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SqsAndLambdaLoadLeveling.SqsAndLambdaLoadLevelingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
