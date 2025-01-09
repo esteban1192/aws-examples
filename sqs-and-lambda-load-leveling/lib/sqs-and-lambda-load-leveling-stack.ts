efbfdec9d518b5594e5240c64c1a2766b77fa43f
esteban1192 <estebanospinasaldarriaga@gmail.com>
2025-01-09 13:02:48 -0500

    api gateway + sqs integration

--- sqs-and-lambda-load-leveling/lib/sqs-and-lambda-load-leveling-stack.ts
+++ sqs-and-lambda-load-leveling/lib/sqs-and-lambda-load-leveling-stack.ts
@@ -2,8 +2,10 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources'
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as path from 'path';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam'
 
 export class SqsAndLambdaLoadLevelingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -23,5 +25,48 @@ export class SqsAndLambdaLoadLevelingStack extends cdk.Stack {
     lambdaFunction.addEventSource(new lambdaEventSources.SqsEventSource(sqsQueue, {
       batchSize: 5,
     }));
+
+    const integrationRole = new iam.Role(this, 'integration-role', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+    });
+
+    const api = new apigateway.RestApi(this, 'SqsApi', {
+      restApiName: 'SQS API',
+      description: 'API Gateway to send messages directly to SQS.',
+    });
+
+    sqsQueue.grantSendMessages(integrationRole)
+
+    const sqsIntegration = new apigateway.AwsIntegration({
+      service: 'sqs',
+      path: `${cdk.Aws.ACCOUNT_ID}/${sqsQueue.queueName}`,
+      integrationHttpMethod: 'POST',
+      options: {
+        credentialsRole: integrationRole,
+        requestParameters: {
+          'integration.request.header.Content-Type': "'application/x-www-form-urlencoded'",
+        },
+        requestTemplates: {
+          'application/json': `Action=SendMessage&MessageBody=$input.body`,
+        },
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseTemplates: {
+              'application/json': `{"message": "Message sent successfully."}`,
+            },
+          },
+        ],
+      },
+    });
+
+    const messages = api.root.addResource('messages');
+    messages.addMethod('POST', sqsIntegration, {
+      methodResponses: [
+        {
+          statusCode: '200',
+        },
+      ],
+    });
   }
 }
